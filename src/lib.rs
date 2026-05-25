5a452ea7dbc4d4f04044980199373023e5bf9ed4
Cristian Marcos Martin <cristianmarcosmartin1@gmail.com>
2026-05-25 23:27:13 +0200

    🧹 Move clippy lints from lib.rs to Cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -13,42 +13,17 @@
 //! To get more info, run `postit -h` or take a look to the README file.
 
 #![warn(
-    clippy::all,
-    clippy::cargo,
-    clippy::nursery,
-    clippy::pedantic,
-    clippy::correctness,
-    clippy::suspicious,
-    clippy::style,
-    clippy::complexity,
-    clippy::perf,
-    clippy::missing_docs_in_private_items,
-    clippy::dbg_macro,
-    clippy::single_match_else,
-    clippy::implicit_clone,
-    clippy::str_to_string,
-    clippy::missing_inline_in_public_items,
-    clippy::unused_trait_names,
-    clippy::wildcard_enum_match_arm,
     clippy::single_call_fn,
-    clippy::as_conversions,
-    clippy::panic,
+    clippy::missing_docs_in_private_items,
+    clippy::missing_inline_in_public_items,
     missing_docs
 )]
 #![allow(
-    clippy::cast_sign_loss,
-    clippy::cast_possible_truncation,
-    clippy::manual_assert,
-    clippy::multiple_crate_versions,
-    clippy::must_use_candidate,
-    clippy::struct_excessive_bools,
     // TMP
     clippy::expect_used,
 )]
 
-pub mod config;
 mod core;
-pub mod docs;
 pub mod models;
 mod persisters;
 
